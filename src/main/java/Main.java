e10068d63dcfdb41d38e88fb30e78c29913ab453
106010266 <leonardegiazaran@gmail.com>
2024-10-30 13:02:56 +0300

    main branch

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,17 @@
+import  java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("egiazaran_branch");
 
+        int num = new Scanner(System.in).nextInt();
+
+        for (int i = 1; i <= num; i++){
+            for (int j = num; j >= 1; j--){
+                if (i * j == num) {
+                    System.out.println(i + " * " + j);
+                }
+            }
+        }
     }
 }
